dc67cb8ce9eeaa40a257755495713cc508f2daa9
anasteishch <83608281+anasteishch@users.noreply.github.com>
2022-01-27 19:24:10 +0200

    2089. Find Target Indices After Sorting Array

---
+++ 2089. Find Target Indices After Sorting Array/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Leetcode._2089._Find_Target_Indices_After_Sorting_Array
+{
+    public class Solution
+    {
+        public IList<int> TargetIndices(int[] nums, int target)
+        {
+            var result = new List<int>();
+            Array.Sort(nums);
+            var start = Array.IndexOf(nums, target);
+            while (start < nums.Length && start != -1)
+            {
+                if(nums[start]==target)
+                    result.Add(start++);
+                else return result;
+            }
+            return result;
+        }
+    }
+}
