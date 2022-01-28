364e2abc37073e97dd4b718b6d08515de80c090c
anasteishch <83608281+anasteishch@users.noreply.github.com>
2022-01-28 16:58:21 +0200

    2114. Maximum Number of Words Found in Sentences

---
+++ 2114. Maximum Number of Words Found in Sentences/Solution_2114.cs
@@ -0,0 +1,20 @@
+﻿namespace Leetcode._2114._Maximum_Number_of_Words_Found_in_Sentences
+{
+    public class Solution_2114
+    {
+        public int MostWordsFound(string[] sentences)
+        {
+            var length = 0;
+            foreach (var sentence in sentences)
+            {
+                var l = sentence.Trim().Split(" ").Length;
+                if (l > length)
+                {
+                    length = l;
+                }
+            }
+
+            return length;
+        }
+    }
+}
